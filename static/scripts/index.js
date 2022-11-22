c4e680a0d8e1fb6e4d7a4e726e139e3bae250e24
Ayush Sharma <ayush.sharma@appsavio.com>
2022-11-22 18:18:14 +0530

    Initial Commit

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,10 +1,12 @@
 function getData() {
-    
-    $.ajax({
-        url: "https://AyuSharma-dev.github.io/ThanksGiving/index.json",
-        dataType: "json"
-    }).done(function(result){
-        console.log(result);
-    });
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("receivedData").innerHTML =this.responseText;
+        }
+    };
+    xhttp.open("GET", "https://AyuSharma-dev.github.io/ThanksGiving/index.json", true);
+    xhttp.send();
 
 }
